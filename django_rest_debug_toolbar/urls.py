3e84dab80ac617a4d5ec0e53686bfd6d3cf6e5dd
Aamish Baloch <aamish.baloch@gmail.com>
2017-08-24 19:27:09 +0500

    Jwt Authentication Implemented

--- django_rest_debug_toolbar/urls.py
+++ django_rest_debug_toolbar/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-refresh/', refresh_jwt_token)
+
 ]
